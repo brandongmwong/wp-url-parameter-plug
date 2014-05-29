0d449220095d1ed6b72a3c70b4ff6702e403f4b5
Brandon Wong <brandon.wong@c02md8a2fd56-l.local>
2014-05-29 10:04:09 -0700

    commit init impound

---
+++ wp-url-parameter-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: URL Parameter
+Description: WP plugin to fix issue with URL parameters being removed server side on Multisite
+*/
+add_action('init', 'url_params', 1);
+
+/*
+*/
+function url_params() {
+    $params = array('preview_id', 'preview_nonce', 'preview', 'promocode');
+    set_url_parameter($params);
+}
+
+function set_url_parameter($params) {
+    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $parseQuery = parse_url($actual_link);
+
+    if (isset($parseQuery[query])) {
+        $queryString = $parseQuery[query];
+        parse_str($queryString, $output);
+
+        foreach ($params as $param) {
+            if (isset($output[$param])) {
+                $_GET[$param] = $output[$param];
+            }
+        }
+    }
+}
+?>
